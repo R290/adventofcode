7b33ad6a787ee89e2b0d83044d548e3d35e4a998
R290 <46033588+R290@users.noreply.github.com>
2022-12-05 16:26:24 +0100

    day 5 part 1

---
+++ day5/day5.py
@@ -0,0 +1,56 @@
+from copy import deepcopy
+
+stacks = []
+
+with open('input','r') as f:
+
+    for line in f:
+
+        # end of initial stack definition
+        if line[1] == '1':
+            break
+        
+        if len(stacks) == 0:
+            stacks = [[] for _ in range(int((len(line)+1)/4))]
+        
+        for i in range(1, len(line), 4):
+            if line[i] != ' ':
+                stacks[i//4].append(line[i])
+
+# reverse stacks
+for i in range(len(stacks)):
+    stacks[i] = stacks[i][::-1]
+
+# stacks = [['Z', 'N'],
+#           ['M', 'C', 'D'],
+#           ['P']]
+
+# copy for part 2
+stacks2 = deepcopy(stacks)
+
+with open('input','r') as f:
+    for line in f:
+        if line[:4] != 'move':
+            continue
+
+        (_, N, _, org, _, dst) = line.rstrip('\n').split(' ')
+
+        org = int(org)-1
+        dst = int(dst)-1
+        N = int(N)
+
+    	# 1
+        for _ in range(N):
+            stacks[dst].append(stacks[org].pop())
+
+        # 2
+        for _ in range(N):
+            stacks2[dst] += stacks2[org][-N:]
+            del stacks2[org][-N:]
+
+print(''.join([stack[-1] for stack in stacks]))
+
+for stack in stacks2:
+
+    if len(stack) > 0:
+        print(stack[-1], end='')
